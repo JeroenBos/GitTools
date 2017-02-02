f3dd32d78ce09ad06e62d83ab2ff1af94a8e17af
JeroenBos <jer123bos@gmail.com>
2017-02-02 21:17:20 +0100

    Implement ExitCodes

---
+++ GitTools/ExitCodes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JBSnorro.GitTools
+{
+	/// <summary>
+	/// Represents the return value of the script, to be interpreted by 'git bisect run'. 
+	/// </summary>
+	public enum ExitCodes
+	{
+		/// <summary> Indicates the current code is good (in the context of a git bisect). </summary>
+		Good = 0,
+		/// <summary> Indicates the current code is bad (in the context of a git bisect). </summary>
+		Bad = 1,
+		/// <summary> Indicates the current code cannot be tested (in the context of a git bisect). </summary>
+		Skip = 125,
+		/// <summary> Indicates the current git bisect run should be aborted. </summary>
+		Abort = 255
+	}
+}
