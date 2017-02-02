3f79d74ff707beb21253b66c905bca1530fecf02
JeroenBos <jer123bos@gmail.com>
2017-02-02 21:10:51 +0100

    Add project files.

---
+++ DeliberatelyFailingTest/Test.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DeliberatelyFailingTest
+{
+	[TestClass]
+	public class Test
+	{
+		/// <summary> 
+		/// This test is supposed to fail at a to be determined commit.
+		/// </summary>
+		[TestMethod]
+		public void DeliberatelyFailingTest()
+		{
+		}
+	}
+}
