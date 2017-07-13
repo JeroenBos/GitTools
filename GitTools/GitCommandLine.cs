67514dd601b29e7d9f610b66f85b86337aef1f4f
JeroenBos <jer123bos@gmail.com>
2017-07-13 13:45:23 +0200

    Implement GitCommandLine.Execute

---
+++ GitTools/GitCommandLine.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JBSnorro.GitTools
+{
+    public static class GitCommandLine
+    {
+        /// <summary>
+        /// Gets or sets the path of the git executable.
+        /// </summary>
+        public static string GitPath = @"C:\Program Files\Git\bin\git.exe";
+        /// <summary>
+        /// Invokes the specified commands on the specified repository and results the results.
+        /// </summary>
+        public static (IEnumerable<string> result, string error) Execute(string repositoryPath, params string[] commands)
+        {
+            Contract.Requires(!string.IsNullOrEmpty(repositoryPath));
+            Contract.Requires(commands != null);
+
+            if (commands.Length == 0)
+                return (Enumerable.Empty<string>(), null);
+
+            ProcessStartInfo info = new ProcessStartInfo()
+            {
+                FileName = GitPath,
+                WorkingDirectory = repositoryPath,
+                CreateNoWindow = true,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+            };
+
+            List<string> results = new List<string>();
+
+            // invoke the commands
+            using (Process gitProcess = new Process())
+            {
+                foreach (string command in commands)
+                {
+                    info.Arguments = command;
+
+                    gitProcess.StartInfo = info;
+                    gitProcess.Start();
+
+                    string error = gitProcess.StandardError.ReadToEnd();
+                    results.Add(gitProcess.StandardOutput.ReadToEnd());
+
+                    gitProcess.WaitForExit();
+                    if (!string.IsNullOrEmpty(error))
+                        return (results, error);
+                }
+            }
+
+            return (results, null);
+        }
+    }
+}
