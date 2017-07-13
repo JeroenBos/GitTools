a60391824a146031346804e8302cfe4c700904f0
JeroenBos <jer123bos@gmail.com>
2017-07-13 12:29:57 +0200

    Add CI project

---
+++ CI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JBSnorro.GitTools.CI
+{
+    /// <summary>
+    /// This program copies a solution, builds it and runs all its tests.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
