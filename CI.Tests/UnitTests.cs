052cc43e4629d587e18bfbed0667f3d7940f639a
JeroenBos <jer123bos@gmail.com>
2017-12-14 08:31:12 +0100

    Add Tests.CI

---
+++ CI.Tests/UnitTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.CI
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+
+        }
+        [TestMethod, ExpectedException(typeof(DivideByZeroException))]
+        public void TestMethod()
+        {
+            throw new DivideByZeroException();
+        }
+    }
+}
