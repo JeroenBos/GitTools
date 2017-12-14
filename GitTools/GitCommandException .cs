4ba878ab5d13ea13876bdd49d5e7540cfa7c543d
JeroenBos <jer123bos@gmail.com>
2017-12-14 18:32:03 +0100

    Add GitCommandException

---
+++ GitTools/GitCommandException .cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JBSnorro.GitTools
+{
+    /// <summary>
+    /// Represents that an error occurred when letting git execute a command.
+    /// </summary>
+    public class GitCommandException : Exception
+    {
+        public GitCommandException() : this("Git errored when executing the command")
+        {
+        }
+
+        public GitCommandException(string error) : base(error)
+        {
+        }
+
+        public GitCommandException(string error, Exception innerException) : base(error, innerException)
+        {
+        }
+    }
+}
