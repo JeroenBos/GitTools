1c27e35a97fa80490e79f50f594f0cc24cefcf2d
JeroenBos <jer123bos@gmail.com>
2017-12-28 04:03:52 +0100

    Make estimate on last n successful commits, rather than all

--- CI/TimingEstimator.cs
+++ CI/TimingEstimator.cs
@@ -9,6 +9,7 @@ namespace JBSnorro.GitTools.CI
     public sealed class TimingEstimator
     {
         public const int UnknownEstimate = int.MinValue;
+        private const int MovingAverageSize = 8;
         public int Estimate
         {
             get
@@ -24,8 +25,15 @@ namespace JBSnorro.GitTools.CI
         {
             if (result == TestResult.Success)
             {
-                this.estimate = (estimate * estimateMultiplicity + timing) / (estimateMultiplicity + 1);
-                this.estimateMultiplicity++;
+                if (estimateMultiplicity == MovingAverageSize)
+                {
+                    this.estimate = (estimate * (MovingAverageSize - 1) + timing) / MovingAverageSize;
+                }
+                else
+                {
+                    this.estimate = (estimate * estimateMultiplicity + timing) / (estimateMultiplicity + 1);
+                    this.estimateMultiplicity++;
+                }
             }
         }
     }
